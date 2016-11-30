ff5eef0093077ab5d998428eb85451e94cd60d14
Cecilia Schiebel <ceciliaschiebel@gmail.com>
2016-11-30 16:58:52 -0300

    update angular version

--- js/app.js
+++ js/app.js
@@ -1,31 +1,37 @@
 /* global angular */
 angular.module('GardenApp', [])
 .controller('gardenCtrl', [ '$scope', '$http', function ($scope, $http) {
-    // set ubicacion
-    // set horario de recordatorio
-    // preguntar si en dia actual llueve
-    // if false mostrar recordatorio para regar
-    // if true mostrar probabilidades de precipitacion
-  console.log('js cargado')
+  // TASKS:
+  // get location
+  // set location
+  // get forecast
+  // get reminder message
+  // set water time
+  // check if water time
+  // launch reminder
+  // if not DONE re launch reminder
+  // display UI data
 
   var forecast
+  var currentLocation = '-27.9218,-55.7542'
 
-  $http.get('https://api.darksky.net/forecast/4874c7d1304dee63c10ffb045c76da1b/-27.9218,-55.7542?&lang=es&units=si')
-    // for later testing ..
-    // .then(function (response) {
-    //   console.log(response.data)
-    // }, function (response) {
-    //   console.log('status: ', response.status, 'statusText: ', response.statusText)
-    // })
-    .then(function (response) {
-      forecast = response.data
-      console.log('forecast', forecast)
-      if (forecast.currently.precipProbability === 0) {
-        console.log('No te olvides de regar las plantas')
-      } else {
-        console.log('Hoy hay probabilidades de lluvia')
-      }
-    }, function (response) {
-      console.log('Algo salio mal. Status: ', response.status, 'statusText: ', response.statusText)
-    })
+  var getForecast = function (location, callback) {
+    $http.jsonp('https://api.darksky.net/forecast/4874c7d1304dee63c10ffb045c76da1b/' + location + '?&lang=es&units=si&callback=JSON_CALLBACK')
+      .then(function (response) {
+        forecast = response.data
+        console.log('forecast', forecast)
+        callback(forecast)
+      }, function (response) {
+        console.log('Algo salio mal. Status: ', response.status, 'statusText: ', response.statusText)
+      })
+  }
+
+  var getReminderMessage = function (currentForecast) {
+    if (currentForecast.currently.precipProbability === 0) {
+      console.log('No te olvides de regar las plantas')
+    } else {
+      console.log('Hoy hay probabilidades de lluvia')
+    }
+  }
+  getForecast(currentLocation, getReminderMessage)
 }])
