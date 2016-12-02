35712baa392bbff719859e5aadaa274fe02303ad
Cecilia Schiebel <ceciliaschiebel@gmail.com>
2016-12-02 09:03:22 -0300

    divide functions to get forecast and reminder msg

--- js/app.js
+++ js/app.js
@@ -4,34 +4,62 @@ angular.module('GardenApp', [])
   // TASKS:
   // get location
   // set location
-  // get forecast
-  // get reminder message
-  // set water time
   // check if water time
   // launch reminder
   // if not DONE re launch reminder
   // display UI data
+  // set water time
 
-  var forecast
-  var currentLocation = '-27.9218,-55.7542'
+  // DONE
+  // get forecast
+  // get reminder message
 
+  var $ctrl = this
+  var forecast = ''
+  // var currentTime = new Date()
+
+  $ctrl.today = {
+    summary: '',
+    precipProbability: '',
+    reminderMsg: ''
+  }
+  $ctrl.setted = {
+    cityName: 'Apóstoles, Misiones',
+    currentLocation: '-27.9218,-55.7542',
+    waterHourString: '',
+    waterHour: ''
+  }
+
+  // gets weather data and saves current day summary and precipitation Probability
   var getForecast = function (location, callback) {
     $http.jsonp('https://api.darksky.net/forecast/4874c7d1304dee63c10ffb045c76da1b/' + location + '?&lang=es&units=si&callback=JSON_CALLBACK')
       .then(function (response) {
         forecast = response.data
+
+        $ctrl.today.summary = forecast.daily.data[0].summary
+        $ctrl.today.precipProbability = forecast.daily.data[0].precipProbability
+
         console.log('forecast', forecast)
-        callback(forecast)
+
+        callback()
       }, function (response) {
         console.log('Algo salio mal. Status: ', response.status, 'statusText: ', response.statusText)
       })
   }
 
-  var getReminderMessage = function (currentForecast) {
-    if (currentForecast.currently.precipProbability === 0) {
-      console.log('No te olvides de regar las plantas')
+  var getReminderMessage = function () {
+    if ($ctrl.today.precipProbability === 0) {
+      $ctrl.today.reminderMsg = 'No te olvides de regar las plantas'
     } else {
-      console.log('Hoy hay probabilidades de lluvia')
+      $ctrl.today.reminderMsg = 'Hoy hay probabilidades de lluvia'
     }
   }
-  getForecast(currentLocation, getReminderMessage)
+
+  var setWaterHourOnSunset = function (currentForecast) {
+    $ctrl.setted.waterHour = new Date(currentForecast.daily.data[0].sunsetTime * 1000)
+    $ctrl.setted.waterHourString = $ctrl.setted.waterHour.getHours() + ':' + $ctrl.setted.waterHour.getMinutes()
+  }
+
+  // init
+  getForecast($ctrl.setted.currentLocation, getReminderMessage)
 }])
